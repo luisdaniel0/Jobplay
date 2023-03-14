e82f0789bb600b1a5c3f8c1b124590681fc244aa
Danny Yu <tiramisu@Dannys-MBP.home>
2023-03-14 14:10:07 -0400

    login

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 import { Routes, Route } from "react-router-dom";
 // import DashBoard from "./DashBoard.jsx";
-// import Login from "./Login.jsx";
+import Login from "./pages/Login/Login";
 // import Register from "./Register.jsx";
 import ProtectedRoutes from "./components/Protection/ProtectedRoutes";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -14,11 +14,11 @@ function App() {
     <div className="App">
      <NavBS />
       <Routes>
-        <Route element={<ProtectedRoutes />}>
-          {/* <Route path="/" element={<DashBoard />} /> */}
-        </Route>
-        {/* <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Register />} /> */}
+        {/* <Route element={<ProtectedRoutes />}>
+          <Route path="/" element={<Login />} />
+        </Route> */}
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="/signup" element={<Register />} /> */}
       </Routes>
     </div>
   );
