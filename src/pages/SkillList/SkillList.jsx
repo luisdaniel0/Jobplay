e5d1bf07bfa2f723c26329ea2db7f3ee3e0276e4
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-03-14 22:42:10 -0400

    add skill components

--- src/pages/SkillList/SkillList.jsx
+++ src/pages/SkillList/SkillList.jsx
@@ -1,42 +1,69 @@
 import { useState, useEffect } from 'react'
-import './SkillList.css'
-import { index, create, update  } from '../../services/skillService'
-import { Container, Row, Col, Card } from 'react-bootstrap'
+import { index, createSkill, update  } from '../../services/skillService'
+import { Col, Card } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
+import './SkillList.css'
+import AddSkillBtn from '../../components/AddSkillBtn/AddSkillBtn'
 
 const SkillList = () => {
   const navigate = useNavigate()
   const [skills, setSkills] = useState([])
 
-  useEffect(() => {
-    const fetchAllSkills = async () => {
-      const data = await index()
-      setSkills([newSkill, ...skills])
-      navigate
-    }
-    fetchAllSkills()
-  }, [])
+  const loaded = () => {
+    let allSkills = skills.map(skill => {
+      return (
+        <div key={skill._id}>
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Title>{skill.skillName}</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+        </div>
+      )
+    })
 
+    return (
+      <>
+        {allSkills}
+      </>
+    )
+  }
+
+  const loading = () => {
+    return (
+      <div>
+        There are no skills...
+      </div>
+    )
+  }
+
+  
+  const handleAddSkill = async (skillData) => {
+    const newSkill  = await createSkill(skillData)
+    setSkills([newSkill, ...skills])
+  }
+  
   const handleUpdateSkill = async (skillData) => {
     const updatedSkill = await update(skillData)
     setSkills(skills.map((skill) => skillData._id === skill._id ? updatedSkill : skill))
     navigate('/skills')
   }
 
+  useEffect(() => {
+    const fetchAllSkills = async () => {
+      const data = await index()
+      setSkills(data)
+    }
+    fetchAllSkills()
+  }, [])
+
   return (
-    <Container>
-      <Row>
-        {skills.map((skill) => (
-          <Col key={skill.id}>
-            <Card>
-              <Card.Body>
-                <Card.Title>{skill.name}</Card.Title>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </Container>
+    <div>
+      <AddSkillBtn handleAddSkill={handleAddSkill}/>
+      {skills ? loaded() : loading()}
+    </div>
   )
 }
 
