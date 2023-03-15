6f18f547e7c3e099c364e81ddccee80dafb4701d
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-03-15 10:35:27 -0400

    remove new skill

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import Register from "./pages/Register/Register";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBS from "./components/Navbar/Nav.jsx"
 import Jobs from "./pages/Jobs/Jobs";
-import NewSkill from "./pages/NewSkill/NewSkill"
 import SkillList from "./pages/SkillList/SkillList";
 import NewSkill from "./components/AddSkillModal/AddSkillModal";
 import NetworkList from "./pages/NetworkList/NetworkList";
@@ -23,7 +22,6 @@ function App() {
       <Routes>
         <Route element={<ProtectedRoutes />}>
           <Route path="/skills" element={<SkillList />} />
-          <Route path="/skills/new" element={<NewSkill />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/network" element={<NetworkList />} />
           <Route path="/jobs/" element={<Jobs />} />
